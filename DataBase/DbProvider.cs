89052885c6a68a7c6643bf11046d0f1e007549a7
даниил миренский <bucashechca@gmail.com>
2020-03-07 19:33:59 +0300

    LiteDb connection fix

---
+++ DataBase/DbProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using LiteDB;
+
+namespace VkDiskCore.DataBase
+{
+    public static class DbProvider
+    {
+        private static readonly object Locker = new object();
+
+        public static LiteDatabase GetDb
+        {
+            get
+            {
+                lock (Locker)
+                {
+                    try
+                    {
+                        return new LiteDatabase(VkDisk.LiteDbConnectionString);
+                    }
+                    catch (Exception)
+                    {
+                        Thread.Sleep(100);
+                        return GetDb;
+                    }
+
+                }
+            }
+        }
+    }
+}
