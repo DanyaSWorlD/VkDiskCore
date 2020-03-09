10beef3b87edb23a8a2853084dce1b82af417e47
даниил миренский <bucashechca@gmail.com>
2020-03-09 15:31:49 +0300

    db provider edit

--- DataBase/DbProvider.cs
+++ DataBase/DbProvider.cs
@@ -9,25 +9,6 @@ namespace VkDiskCore.DataBase
     {
         private static readonly object Locker = new object();
 
-        public static LiteDatabase GetDb
-        {
-            get
-            {
-                // figth the file is busy exception
-                lock (Locker)
-                {
-                    try
-                    {
-                        return new LiteDatabase(VkDisk.LiteDbConnectionString);
-                    }
-                    catch (IOException e)
-                    {
-                        Thread.Sleep(100);
-                        Console.WriteLine(e.Message);
-                        return GetDb;
-                    }
-                }
-            }
-        }
+        public static LiteDatabase GetDb => new LiteDatabase(VkDisk.LiteDbConnectionString);
     }
 }
